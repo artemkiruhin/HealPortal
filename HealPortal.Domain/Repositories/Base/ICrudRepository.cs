8f342a863bba7e2df0b89905e37592eda6567619
artemkiruhin <temkaunik@gmail.com>
2025-01-07 22:05:19 +0300

    Base CRUD Repo

---
+++ HealPortal.Domain/Repositories/Base/ICrudRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace HealPortal.Domain.Repositories.Base;
+
+public interface ICrudRepository<TEntity>
+{
+    Task<TEntity?> GetByIdAsync(Guid id);
+    Task<IEnumerable<TEntity>> GetAllAsync();
+    Task<TEntity> AddAsync(TEntity entity);
+    Task<TEntity> UpdateAsync(TEntity entity);
+    Task<TEntity> DeleteAsync(TEntity entity); 
+    Task<List<TEntity>> AddRangeAsync(IEnumerable<TEntity> entities);
+}
