3839dfe0eb3e29223731b336c66ea7878cd13ef8
artemkiruhin <temkaunik@gmail.com>
2025-01-07 22:13:40 +0300

    Post Base Repo

---
+++ HealPortal.Domain/Repositories/IPostEntity.cs
@@ -0,0 +1,17 @@
+﻿using HealPortal.Domain.Models.Entities;
+using HealPortal.Domain.Repositories.Base;
+
+namespace HealPortal.Domain.Repositories;
+
+public interface IPostRepository : ICrudRepository<PostEntity>
+{
+    Task<IEnumerable<PostEntity>> GetByTitleAsync(string title);
+    Task<IEnumerable<PostEntity>> GetByContentAsync(string content);
+    Task<IEnumerable<PostEntity>> GetByUserIdAsync(Guid userId);
+    Task<IEnumerable<PostEntity>> GetByTagsAsync(string[] tags);
+    Task<IEnumerable<PostEntity>> GetByCreatedDateAsync(bool ascending = true);
+    Task<IEnumerable<PostEntity>> GetByDurationAsync(uint durationInMinutes);
+    Task<IEnumerable<PostEntity>> GetByWatchersAsync(uint watchers, bool ascending = false);
+    Task<IEnumerable<PostEntity>> GetByLikesAsync(uint likes, bool ascending = true);
+    Task<IEnumerable<PostEntity>> GetByDislikesAsync(uint dislikes, bool ascending = true);
+}
