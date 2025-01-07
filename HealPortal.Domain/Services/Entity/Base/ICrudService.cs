37cba8eb799ef20566222429de7e737a59399d07
artemkiruhin <temkaunik@gmail.com>
2025-01-07 22:22:42 +0300

    Base crud service

---
+++ HealPortal.Domain/Services/Entity/Base/ICrudService.cs
@@ -0,0 +1,8 @@
+﻿namespace HealPortal.Domain.Services.Entity.Base;
+
+public interface ICrudService<TDto>
+{
+    Task<TDto?> GetByIdAsync(Guid id);
+    Task<IEnumerable<TDto>> GetAllAsync();
+    Task<bool> DeleteAsync(Guid id); 
+}
