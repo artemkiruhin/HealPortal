c0650de8f68128c97e2a667dec4891e6bd8c8364
artemkiruhin <temkaunik@gmail.com>
2025-01-07 22:07:56 +0300

    User Base Repo

---
+++ HealPortal.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using HealPortal.Domain.Models.Entities;
+using HealPortal.Domain.Repositories.Base;
+
+namespace HealPortal.Domain.Repositories;
+
+public interface IUserRepository : ICrudRepository<UserEntity>
+{
+    Task<UserEntity?> GetByUsernameAsync(string username);
+    Task<UserEntity?> GetByUsernameAndPasswordHashAsync(string username, string passwordHash);
+}
