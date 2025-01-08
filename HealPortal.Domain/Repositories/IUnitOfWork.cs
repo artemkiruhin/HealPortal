7e6941cef3c7fbdab7cb3f5f20de5f32036fc2b5
artemkiruhin <temkaunik@gmail.com>
2025-01-08 18:46:12 +0300

    UoW interface

---
+++ HealPortal.Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿namespace HealPortal.Domain.Repositories;
+
+public interface IUnitOfWork : IDisposable
+{
+    ICommentRepository CommentRepository { get; }
+    IPostRepository PostRepository { get; }
+    IUserRepository UserRepository { get; }
+    ITagRepository TagRepository { get; }
+    Task<int> SaveChangesAsync();
+    Task BeginTransactionAsync();
+    Task CommitTransactionAsync();
+    Task RollbackTransactionAsync();
+}
