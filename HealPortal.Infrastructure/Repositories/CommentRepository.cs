5723eea4a8af3275ac7859b285be12e1d035016a
artemkiruhin <temkaunik@gmail.com>
2025-01-08 19:56:35 +0300

    Comment repo

---
+++ HealPortal.Infrastructure/Repositories/CommentRepository.cs
@@ -0,0 +1,12 @@
+﻿using HealPortal.Domain.Models.Entities;
+using HealPortal.Domain.Repositories;
+using HealPortal.Infrastructure.Repositories.Base;
+
+namespace HealPortal.Infrastructure.Repositories;
+
+public class CommentRepository : BaseCrudRepository<CommentEntity>, ICommentRepository
+{
+    public CommentRepository(IUnitOfWork database, AppDbContext context) : base(database, context)
+    {
+    }
+}
