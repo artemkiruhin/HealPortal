7f8b35d0256ab08d228b4a0e3d42f15b209cd2e5
artemkiruhin <temkaunik@gmail.com>
2025-01-08 20:04:56 +0300

    SHA 256 hasher

---
+++ HealPortal.Services/Serives/Hasher/Sha256Hasher.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using HealPortal.Domain.Services;
+using static System.Security.Cryptography.SHA256;
+
+namespace HealPortal.Services.Serives.Hasher;
+
+public class Sha256Hasher : IHashService
+{
+    public string Hash(string message)
+        => Convert.ToBase64String(SHA256.HashData(Encoding.UTF8.GetBytes(message)));
+}
