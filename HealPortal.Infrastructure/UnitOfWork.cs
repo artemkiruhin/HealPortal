88d3167ae30df1552712da922cc0565a7a7c04ea
artemkiruhin <temkaunik@gmail.com>
2025-01-08 18:49:56 +0300

    UoW impl

---
+++ HealPortal.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,44 @@
+﻿using HealPortal.Domain.Repositories;
+
+namespace HealPortal.Infrastructure;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+
+    public UnitOfWork (AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+
+    public ICommentRepository CommentRepository { get; }
+    public IPostRepository PostRepository { get; }
+    public IUserRepository UserRepository { get; }
+    public ITagRepository TagRepository { get; }
+    
+    
+    public async Task<int> SaveChangesAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
+
+    public async Task BeginTransactionAsync()
+    {
+        await _context.Database.BeginTransactionAsync();
+    }
+
+    public async Task CommitTransactionAsync()
+    {
+        await _context.Database.CommitTransactionAsync();
+    }
+
+    public async Task RollbackTransactionAsync()
+    {
+        await _context.Database.RollbackTransactionAsync();
+    }
+}
