f3d6d0b0e7f4442a583a6ec6a5bd2f32090f95e6
artemkiruhin <temkaunik@gmail.com>
2025-01-08 19:55:38 +0300

    Tag repo

---
+++ HealPortal.Infrastructure/Repositories/TagRepository.cs
@@ -0,0 +1,12 @@
+﻿using HealPortal.Domain.Models.Entities;
+using HealPortal.Domain.Repositories;
+using HealPortal.Infrastructure.Repositories.Base;
+
+namespace HealPortal.Infrastructure.Repositories;
+
+public class TagRepository : BaseCrudRepository<TagEntity>, ITagRepository
+{
+    public TagRepository(IUnitOfWork database, AppDbContext context) : base(database, context)
+    {
+    }
+}
