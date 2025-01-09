0a5518f6d52ed023e039a11db8373ddfddda0133
artemkiruhin <temkaunik@gmail.com>
2025-01-09 19:05:12 +0300

    User Service Interface

--- HealPortal.Domain/Services/Entity/Base/ICrudService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HealPortal.Domain.Services.Entity.Base;
-
-public interface ICrudService<TDto>
-{
-    Task<TDto?> GetByIdAsync(Guid id);
-    Task<IEnumerable<TDto>> GetAllAsync();
-    Task<bool> DeleteAsync(Guid id); 
-}
---
+++ HealPortal.Domain/Services/Entity/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using HealPortal.Domain.Models.Dtos;
+using HealPortal.Domain.Models.Entities;
+
+namespace HealPortal.Domain.Services.Entity;
+
+public interface IUserService
+{
+    Task<UserDto> GetByIdAsync(Guid id);
+    Task<bool> Registration(string username, string passwordHash, UserRole role = UserRole.User);
+    Task<string> Login(string username, string passwordHash);
+    Task<bool> RemoveImg(Guid id);
+    Task<bool> ChangePassword(Guid id, string oldPassword, string newPassword);
+    Task<bool> SetImg(Guid id, string img);
+}
