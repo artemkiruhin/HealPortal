746166bbb391199ff7f89dc6de4ddf9ba4ba7472
artemkiruhin <temkaunik@gmail.com>
2025-01-09 19:13:04 +0300

    Post service interface

---
+++ HealPortal.Domain/Services/Entity/IPostService.cs
@@ -0,0 +1,17 @@
+﻿using HealPortal.Domain.Models.Dtos;
+
+namespace HealPortal.Domain.Services.Entity;
+
+public interface IPostService
+{
+    Task<bool> CreateAsync(Guid userId, string title, string content, string[] tags, uint durationInMinutes);
+    Task<bool> DeleteAsync(Guid userId, Guid postId);
+    Task<bool> UpdateAsync(Guid userId, Guid postId, string? title, string? content, string[]? tags, uint? durationInMinutes);
+    Task<IEnumerable<PostDto>> GetPostsAsync(Guid userId);
+    Task<IEnumerable<PostDto>> GetPostsAsync();
+    Task<IEnumerable<PostDto>> GetPostsAsync(string[] tags);
+    Task<PostDto> GetByIdAsync(Guid id);
+    Task<bool> LikeAsync(Guid postId);
+    Task<bool> DislikeAsync(Guid postId);
+    Task<bool> WatchAsync(Guid postId);
+}
