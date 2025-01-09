0b10bec2ac4578293108052ce852e8037c1ab030
artemkiruhin <temkaunik@gmail.com>
2025-01-09 19:15:11 +0300

    Tag service interface

---
+++ HealPortal.Domain/Services/Entity/ITagService.cs
@@ -0,0 +1,12 @@
+﻿using HealPortal.Domain.Models.Dtos;
+
+namespace HealPortal.Domain.Services.Entity;
+
+public interface ITagService
+{
+    Task<bool?> GetByIdAsync(Guid id);
+    Task<IEnumerable<TagDto>> GetAllAsync();
+    Task<bool> AddAsync(string name);
+    Task<bool> UpdateAsync(string name);
+    Task<bool> DeleteAsync(Guid id); 
+}
