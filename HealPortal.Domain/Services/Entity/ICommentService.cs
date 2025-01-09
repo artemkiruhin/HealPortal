ec523e7b186920a5548933476d672cb31faad5d8
artemkiruhin <temkaunik@gmail.com>
2025-01-09 19:08:30 +0300

    Comment service interface

---
+++ HealPortal.Domain/Services/Entity/ICommentService.cs
@@ -0,0 +1,12 @@
+﻿using HealPortal.Domain.Models.Dtos;
+
+namespace HealPortal.Domain.Services.Entity;
+
+public interface ICommentService
+{
+    Task<bool> CreateAsync(Guid userId, Guid postId, string content);
+    Task<bool> DeleteAsync(Guid userId, Guid commentId);
+    Task<bool> UpdateAsync(Guid userId, Guid commentId, string content);
+    Task<IEnumerable<CommentDto>> GetCommentsAsync(Guid postId);
+    Task<CommentDto> GetByIdAsync(Guid commentId);
+}
